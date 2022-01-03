b4aeb2cc36a0c4e75e4c8d320eb18d7b1726910a
Adrian Rubak <adek.rubak@gmail.com>
2022-01-03 19:37:01 +0100

    Camera is working created by Wikuś

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,11 +1,76 @@
-import React from 'react';
-import {StyleSheet, Text, View} from "react-native";
-import Camera from '../sensors/Camera'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
-const CameraScreen = () => {
+export default function CameraScreen() {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
+
+
     return (
-            <Camera/>
+        <View style={styles.container}>
+            <Camera style={styles.preview} type={Camera.Constants.Type.back}  ref={ref => {this.camera = ref;}}>
+                <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                    <TouchableOpacity onPress={async () => {
+                        if (this.camera) {
+                            let photo = await this.camera.takePictureAsync();
+                            console.log(photo);
+                        }
+                    }} style={styles.capture}>
+                        <Text style={{ fontSize: 14 }}> SNAP </Text>
+                    </TouchableOpacity>
+                </View>
+            </Camera>
+        </View>
     );
 }
 
-export default CameraScreen;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        flexDirection: 'row',
+        margin: 20,
+    },
+    button: {
+        flex: 0.1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 18,
+        color: 'white',
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20,
+    },
+});
