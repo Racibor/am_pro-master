08b1cf04c468bc9181cd2f6d9faea32c338ecf6b
Adrian Rubak <adek.rubak@gmail.com>
2022-01-23 19:40:49 +0100

    update advertisement Form

--- screens/UpdateAdvertisementForm.js
+++ screens/UpdateAdvertisementForm.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import {NativeBaseProvider} from "native-base/src/core/NativeBaseProvider";
 import {Center, Input, VStack} from "native-base";
-import FormControlLabel from "native-base";
+import FormControlLabel from "native-base/src/components/composites/FormControl/FormControlLabel";
 
-const UpdateAdvertisementForm = () => {
-    // let data = route.params;
-    // const [title, onChangeTextTitle] = React.useState("");
+const UpdateAdvertisementForm = ({route}) => {
+    let data = route.params;
+    const [title, onChangeTextTitle] = React.useState(data.title);
     // const [description, onChangeTextDescription] = React.useState("");
-    // const [price, onChangePrice] = React.useState("");
+    const [price, onChangePrice] = React.useState(data.price);
     // const [img, onChangeImg] = useState(null);
 
     return (
         <NativeBaseProvider>
-            <Center flex={1}>
-                {/*<VStack width="90%" mx="3">*/}
-                    {/*<FormControlLabel _text={{bold: true, fontSize: 25}}>Title:</FormControlLabel>*/}
-                    {/*<Input*/}
-                    {/*    value={title}*/}
-                    {/*    onChangeText={onChangeTextTitle}*/}
-                    {/*    placeholder="Title"*/}
-                    {/*/>*/}
-                {/*</VStack>*/}
+            <Center flex={1} bg="tertiary.300">
+                <VStack width="90%" mx="3">
+                    {/*Title*/}
+                    <FormControlLabel _text={{bold: true, fontSize: 20}}>Title:</FormControlLabel>
+                    <Input
+                        size={"lg"}
+                        value={title}
+                        onChangeText={onChangeTextTitle}
+                        placeholder="Title"
+                    />
+                    {/*Price*/}
+                    <FormControlLabel _text={{bold: true, fontSize: 20}}>Price:</FormControlLabel>
+                    <Input
+                        size={"lg"}
+                        value={price}
+                        onChangeText={onChangePrice}
+                        placeholder="Price"
+                        keyboardType="numeric"
+                    />
+                </VStack>
             </Center>
         </NativeBaseProvider>
     );
