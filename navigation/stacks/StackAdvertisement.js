5991ce038ebae12324305b9848d10bfb3e75cc98
Adrian Rubak <adek.rubak@gmail.com>
2022-01-25 00:31:10 +0100

    fix problem with tab bar icon

--- navigation/stacks/StackAdvertisement.js
+++ navigation/stacks/StackAdvertisement.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import FindScreen from "../../screens/FindScreen";
-import LoginScreen from "../../screens/LoginScreen";
 import AdvertisementDetails from "../../screens/AdvertisementDetails";
 
 const Stack = createNativeStackNavigator();
